e3db1224ea8a160aa1b18d3ed09cca1cd3b278e3
Matthew Sherman <sherman.matthew.p@gmail.com>
2016-03-27 16:17:30 -0400

    Initial Commit

---
+++ NexStar.NET/NexStarConnectionManger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NexStar.NET.Exceptions;
+
+namespace NexStar.NET
+{
+    public static class NexStarConnectionManager
+    {
+        private static Dictionary<Guid, SerialPort> _registeredSerialPorts;
+
+        /// <summary>
+        /// Contains tools for managing connections to the nex star device, and providing factory functionality for the API object
+        /// </summary>
+        static NexStarConnectionManager()
+        {
+            _registeredSerialPorts = new Dictionary<Guid, SerialPort>();
+        }
+         
+        public static Guid RegisterNexStarDevice(SerialPort nexStarPortToRegister)
+        {
+            Guid newPortKey = Guid.NewGuid();
+            _registeredSerialPorts.Add(newPortKey, nexStarPortToRegister);
+            return newPortKey;
+        }
+
+        public static void UnregisterNexStarDevice(Guid portKey)
+        {
+            try
+            {
+                _registeredSerialPorts.Remove(portKey);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidNexStarPortKeyException("The port key is not current registered.", ex);
+            }
+        }
+    }
+}
---
+++ NexStar.NET/SerialMgmt/SerialPortController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexStar.NET.SerialMgmt
+{
+    class SerialPortController
+    {
+        SerialPort _managedPort;
+        public SerialPortController(SerialPort portToManage)
+        {
+            _managedPort = portToManage;
+        }
+
+
+    }
+}
